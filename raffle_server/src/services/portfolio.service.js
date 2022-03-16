08529f1376ce7548f20a3abf5b902f82c9f452b4
yewonbahn <63235104+yewonbahn@users.noreply.github.com>
2022-03-17 00:19:51 +0900

    #9 model 명 변경

--- raffle_server/src/services/portfolio.service.js
+++ raffle_server/src/services/portfolio.service.js
@@ -5,16 +5,20 @@ const axios = require('axios');
 const config = require('../config/config');
 
 const get_moralis_nft = async (body) => {
- 
-  await axios.get('https://deep-index.moralis.io/api/v2/0x13c150622405bf2b5759663ce811b2b87f053601/nft?chain=eth&format=decimal',{
-    method: 'GET',
+  var chain_type;
+  if(chain_id==1){
+    chain_type = 'eth'
+  }
+  await axios.get(`https://deep-index.moralis.io/api/v2/${wallet}/nft/chain=${chain_type}&format=decimal`,{
     headers: {
       'x-api-key': config.moralis.secret
     }
   }) .then((Response)=>{
-    res.status(httpStatus.OK).send(Response.data);
-  }) 
-  .catch((Error)=>{console.log(Error)});  
+    NFT.createTx(Response.data);
+    console.log(Response.data);
+  }) .catch((Error)=>{console.log(Error)});  
+
+
 };
 
 const getNFTTransaction = async (wallet, chain_id) => {
@@ -22,7 +26,7 @@ const getNFTTransaction = async (wallet, chain_id) => {
   if(chain_id==1){
     chain_type = 'eth'
   }
-  await axios.get(`https://deep-index.moralis.io/api/v2/${wallet}/nft/transfers?${chain_type}&format=decimal`,{
+  await axios.get(`https://deep-index.moralis.io/api/v2/${wallet}/nft/transfers?chain=${chain_type}&format=decimal`,{
     headers: {
       'x-api-key': config.moralis.secret
     }
