9aa04db94328da6b9e4a177428e30cafff973e97
web3protocol <kissonmew@gmail.com>
2022-03-16 23:05:13 +0900

    #9 모랄리스 키 .env파일로 이동

--- raffle_server/src/config/config.js
+++ raffle_server/src/config/config.js
@@ -25,6 +25,7 @@ const envVarsSchema = Joi.object()
     SMTP_USERNAME: Joi.string().description('username for email server'),
     SMTP_PASSWORD: Joi.string().description('password for email server'),
     EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+    MORALIS_SECRET: Joi.string().description('Moralis secret key'),
   })
   .unknown();
 
@@ -60,4 +61,7 @@ module.exports = {
     },
     from: envVars.EMAIL_FROM,
   },
+  moralis: {
+    secret : envVars.MORALIS_SECRET
+  }
 };
--- raffle_server/src/services/portfolio.service.js
+++ raffle_server/src/services/portfolio.service.js
@@ -2,19 +2,19 @@ const httpStatus = require('http-status');
 const { User, WalletEth } = require('../models');
 const ApiError = require('../utils/ApiError');
 const axios = require('axios');
-const createUser = async (body) => {
+const config = require('../config/config');
+
+const get_moralis_nft = async (body) => {
  
-    var returndata ="";
-    axios.get('https://deep-index.moralis.io/api/v2/0x13c150622405bf2b5759663ce811b2b87f053601/nft?chain=eth&format=decimal',{
-        method: 'GET',
-        headers: {
-          'x-api-key': 'ob3tEdcftYbkpUiInzVe2pn2yO7ncdqZOomeymK2f69lIjFrbd4b3zyGLOHwylEr'
-        }
-      }) .then((Response)=>{
-        returndata = JSON.stringify(Response.data)
-        
-      }) 
-      .catch((Error)=>{console.log(Error)})
+  await axios.get('https://deep-index.moralis.io/api/v2/0x13c150622405bf2b5759663ce811b2b87f053601/nft?chain=eth&format=decimal',{
+    method: 'GET',
+    headers: {
+      'x-api-key': config.moralis.secret
+    }
+  }) .then((Response)=>{
+    res.status(httpStatus.OK).send(Response.data);
+  }) 
+  .catch((Error)=>{console.log(Error)});
       
       
 };
