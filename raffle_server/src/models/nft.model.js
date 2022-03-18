5b5dd49568efcaa41207889532307ac0402c7c38
web3protocol <kissonmew@gmail.com>
2022-03-19 02:24:15 +0900

    #8 merged branches

--- raffle_server/src/models/nft.model.js
+++ raffle_server/src/models/nft.model.js
@@ -17,7 +17,6 @@ const nftcreate= async (data,wallet) => {
     var langs = [nft_coll_id, splittedAddr,symbol,name,contract_type];
     var res = langs.join(',');
     finaltuple+=",("+res+")";
-<<<<<<< HEAD
   };
   console.log(finaltuple.slice(10) );
   try {
@@ -105,54 +104,5 @@ const createTx_tuple= (data) =>{
 module.exports = {
     nft,
     createTx,
-=======
-
-    const nft_item_id = '\"'+uuidv4.v1()+'\"';
-    const token_id='\"'+data.result[idx].token_id+'\"';
-    const owner_of='\"'+wallet+'\"';
-    const meatadata= '\"'+data.result[idx].meatadata+'\"';
-    const frozen = '\"'+data.result[idx].frozen+'\"';
-    const block_number = '\"'+data.result[idx].block_number+'\"';
-    const splittedAddr2 = owner_of.replace('0x','');
-    var langs2 = [nft_item_id,nft_coll_id,token_id,splittedAddr2,meatadata,frozen,block_number];
-    var res2 = langs2.join(',');
-
-    finaltuple2+=",("+res2+")";
-};
-console.log(finaltuple.slice(10));
-
-console.log(finaltuple2.slice(10));
-try {
-  conn = await pool.getConnection();
-
-  const sql = 'INSERT INTO tb_nft_collection_eth (nft_coll_id, token_address, symbol, name, contract_type) VALUES '+ finaltuple.slice(10);
-
-  const sql2 = 'INSERT INTO tb_nft_eth (nft_item_id ,nft_coll_id, token_id, owner_of, metadata, frozen,block_number) VALUES '+ finaltuple2.slice(10);
-  console.log(sql);
-  console.log(sql2);
-  const dbRes = await conn.query(sql);
-  const dbRes2 = await conn.query(sql2);
-
-
-  console.log(dbRes);//성공 리턴
-  return dbRes,dbRes2;
-  
-}catch(err) {
-  console.log(err);
-  return false;
-}
- finally {
-    if (conn) conn.release(); //release to pool
-}
-
-}
-
-
-module.exports = {
-    
-    nftcreate
-
-    
->>>>>>> 94cdec1975b356322c2a5ee5f2bf93d5e440984c
 };
   
