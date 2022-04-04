efd019ba379f4c07f1cca6c510a91cafa618c97a
yewonbahn <banyaewon98@gmail.com>
2022-04-04 18:17:31 +0900

    #6 get_user(지갑주소,프로필사진,닉네임) get 완료

--- raffle_server/src/models/portfolio.model.js
+++ raffle_server/src/models/portfolio.model.js
@@ -4,29 +4,35 @@ const logger = require('../config/logger');
 const pool = require('./plugins/dbHelper');
 let conn;
 
-const get_user= async (user_wallet) => {
+const get_user= async (query) => {
     //TODO
     //우리 디비에서 유저정보가져와주기
+    var wallet ={
+      address : query.user_id_or_address,
+      chain_id : query.chain_id
+    }
     var rows;
-    const splittedAddr = user_wallet.replace('0x','');
-    // try {
-    //   conn = await pool.getConnection();
+    const splittedAddr = wallet.address.replace('0x','');
+    try {
+      conn = await pool.getConnection();
   
-    //   //TODO 체인아이디 따라 디비테이블 분기 넣을것 
-    //   if(wallet.chain_id==1){
+      //TODO 체인아이디 따라 디비테이블 분기 넣을것 
+      if(wallet.chain_id==1){
       
-    //     const query ="SELECT tb_user_eth.nickname FROM tb_wallet_eth INNER JOIN tb_user ON tb_wallet_eth.wallet_id = tb_user.wallet_id WHERE tb_wallet_eth.address=?"
-    //     rows = await conn.query(query, splittedAddr);
-    //     if(rows[0] == undefined){
-    //         return false;
-    //     }else{
-    //         // console.log(rows[0]);
-    //         return rows[0];//TODO 양식맞추기
-    //     }
-    //   }
-    // } finally {
-    //     if (conn) conn.release();
-    // }    
+        const query ="SELECT tb_user.nickname,tb_user.profile_pic FROM tb_wallet_eth INNER JOIN tb_user ON tb_wallet_eth.wallet_id = tb_user.wallet_id WHERE tb_wallet_eth.address=?"
+        rows = await conn.query(query, splittedAddr);
+        rows[0].address=wallet.address;
+        console.log(rows[0]);
+        if(rows[0] == undefined){
+            return false;
+        }else{
+            console.log(rows[0]);
+            return rows[0];//TODO 양식맞추기
+        }
+      }
+    } finally {
+        if (conn) conn.release();
+    }    
 }
 
 
