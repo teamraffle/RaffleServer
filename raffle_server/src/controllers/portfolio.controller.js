a73165bac6ed0a0ec8ec06b60a6039381e8ae526
web3protocol <kissonmew@gmail.com>
2022-04-11 23:48:48 +0900

    #33 GET /v1/portfolios/basic 404 test

--- raffle_server/src/controllers/portfolio.controller.js
+++ raffle_server/src/controllers/portfolio.controller.js
@@ -6,8 +6,7 @@ const { nftService,portfolioService } = require('../services');
 
 const get_portfolio = catchAsync(async (req, res, next) => {
   
-  //TDOO 쿼리 인풋이 address 일때만 구현됐음
-  // 유저아이디일때 분기점 필요함
+  //TDOO address 일때만 구현됐음
   const portfolio= await portfolioService.get_user_info(req.query);
   res.status(httpStatus.OK).send(portfolio);
 
--- raffle_server/tests/integration/portfolio.test.js
+++ raffle_server/tests/integration/portfolio.test.js
@@ -11,21 +11,6 @@ setupTestDB();
 
 describe('Portfolio routes', () => {
     
-    // let samplePortfolio = {
-    //     wallet_address : '0xB999D5cb1868368766c41c0F455a9243B2688CF3',
-    //     nft_hodings: 0,
-    //     collections_holdings:0,
-    //     av_holding_period:43.25,
-    //     most_collection_name: faker.name.findName(),
-    //     most_collection_icon: faker.image.avatar(),
-    //     est_market_value:0.053,
-    //     holding_volume:0,
-    //     earnings_rate:0,
-    //     total_gas_fee: 1,
-    //     buy_volume: 0.076,
-    //     sell_volume: 0.34,
-    //     create_timestamp: "2022-04-11 12:30:01.000"
-    // }
     beforeAll(async () => {
         input = {
             chain_id : 1,
@@ -43,60 +28,63 @@ describe('Portfolio routes', () => {
       });
 
     describe('GET /v1/portfolios/basic', () => {
-      test('should return 200 and return portfolio. Search with ADDRESS', async () => {
-        //8초는 기다려야 포폴완성됨
-        await new Promise((resolve) => setTimeout(() => resolve(true), 8000));
+        test('should return 404 because no data', async () => {
 
-        const res = await request(app)
-          .get('/v1/portfolios/basic')
-          .set('Content-Type', "application/json")
-          .query(
-              {
-                chain_id : 1,
-                user_id_or_address: userSmall.wallet.address,
-              }
-          )
-          .send()
-          .expect(httpStatus.OK);
-        
-          expect(res.body).toEqual({
-            updated_at: expect.anything(),
-            user: {
-              nickname: userSmall.nickname,
-              user_id: userSmall.user_id,
-              profile_pic: userSmall.profile_pic
-            },
-            portfolio: {
-              wallet_address: userSmall.wallet.address,
-              nft_holdings: expect.anything(),
-              collections_holdings: expect.anything(),
-              av_holding_period: expect.anything(),
-              most_collection_name: expect.anything(),
-              most_collection_icon: expect.anything(),
-              est_market_value: expect.anything(),
-              holding_volume: expect.anything(),
-              earnings_rate: expect.anything(),
-              total_gas_fee: expect.anything(),
-              buy_volume: expect.anything(),
-              sell_volume: expect.anything()
-            }
+            await request(app)
+              .get('/v1/portfolios/basic')
+              .query(
+                  {
+                    chain_id : 1,
+                    user_id_or_address: '0xccce16Cdc8c47e1E1E7ccaf62a36D0d4ac7B7ccc',
+                  }
+              )
+              .send()
+              .expect(httpStatus.NOT_FOUND);
           });
-         
-      });
+
+
+        test('should return 200 and return portfolio. Search with ADDRESS', async () => {
+            //8초는 기다려야 포폴완성됨
+            await new Promise((resolve) => setTimeout(() => resolve(true), 8000));
+
+            const res = await request(app)
+            .get('/v1/portfolios/basic')
+            .set('Content-Type', "application/json")
+            .query(
+                {
+                    chain_id : 1,
+                    user_id_or_address: userSmall.wallet.address,
+                }
+            )
+            .send()
+            .expect(httpStatus.OK);
+            
+            expect(res.body).toEqual({
+                updated_at: expect.anything(),
+                user: {
+                nickname: userSmall.nickname,
+                user_id: userSmall.user_id,
+                profile_pic: userSmall.profile_pic
+                },
+                portfolio: {
+                wallet_address: userSmall.wallet.address,
+                nft_holdings: expect.anything(),
+                collections_holdings: expect.anything(),
+                av_holding_period: expect.anything(),
+                most_collection_name: expect.anything(),
+                most_collection_icon: expect.anything(),
+                est_market_value: expect.anything(),
+                holding_volume: expect.anything(),
+                earnings_rate: expect.anything(),
+                total_gas_fee: expect.anything(),
+                buy_volume: expect.anything(),
+                sell_volume: expect.anything()
+                }
+            });
+            
+        });
   
-    //   test('should return 404 because no data', async () => {
-    //     test_user_id = await User.create(user, wallet_id);
-    //     await request(app)
-    //       .get('/v1/register/nickname')
-    //       .query(
-    //           {
-    //             chain_id : 1,
-    //             check_value: user.nickname,
-    //           }
-    //       )
-    //       .send()
-    //       .expect(httpStatus.CONFLICT);
-    //   });
+      
   
       
   
