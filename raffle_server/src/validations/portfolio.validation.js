814be9b13e3fb188f1aa262a5afb91f18ab33fd7
yewonbahn <banyaewon98@gmail.com>
2022-04-13 22:15:48 +0900

    #32 GET /v1/portfolios/basic 변수 address만 되게 yaml

--- raffle_server/src/validations/portfolio.validation.js
+++ raffle_server/src/validations/portfolio.validation.js
@@ -3,7 +3,7 @@ const { ethWalletOrUUID } = require('./custom.validation');
 
 const getPortfolio = {
   query: Joi.object().keys({
-    user_id_or_address: Joi.string().required().custom(ethWalletOrUUID),
+    address: Joi.string().required().custom(ethWalletOrUUID),
     chain_id : Joi.number().required().integer(),
     offset: Joi.number().integer(),
     limit: Joi.number().integer(),
@@ -11,6 +11,17 @@ const getPortfolio = {
   }),
 };
 
+
+const activity = {
+  query: Joi.object().keys({
+    address: Joi.string().required().custom(ethWalletOrUUID),
+    chain_id : Joi.number().required().integer(),
+    offset: Joi.number().integer(),
+    limit: Joi.number().integer(),
+    cursor: Joi.string(),
+  }),
+};
 module.exports = {
   getPortfolio,
+  activity
 };
