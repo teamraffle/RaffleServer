80c5b54a561090ead0004b1435916361b4384d38
web3protocol <kissonmew@gmail.com>
2022-04-15 02:38:00 +0900

    #41 모델단위 테스트코드 작성 완 페이지 사이즈 조절 가능해짐

--- raffle_server/tests/unit/model/portfolio.model.test.js
+++ raffle_server/tests/unit/model/portfolio.model.test.js
@@ -10,27 +10,51 @@ setupTestDB();
 describe('Portfolio Model test', () => {
   const _address = '0xC6eAfD3a5A919a17374c5a6d5BcE45CFE4dc76F9';
 
-  test.skip('get nft Basic : Address, Chain id only', async () => {
-    const query = {
-      chain_id: 1,
-      address: _address,
-    };
-    await Portfolio.get_nft(query);
+  describe('NFT', () => {
+    test('get nft Basic : Address, Chain id only', async () => {
+      const query = {
+        chain_id: 1,
+        address: _address,
+      };
+      await Portfolio.get_nft(query);
+    });
+
+    test('get nft Page and Limit option ', async () => {
+      const _limit = 5;
+      const query = {
+        chain_id: 1,
+        address: _address,
+        page: 0,
+        limit: _limit,
+      };
+      const res = await Portfolio.get_nft(query);
+
+      expect(JSON.parse(res).result.length).to.equal(_limit);
+    });
   });
 
-  test('get nft Page and Limit option ', async () => {
-    const _limit = 5;
-    const query = {
-      chain_id: 1,
-      address: _address,
-      page: 0,
-      limit: _limit,
-    };
-    const res = await Portfolio.get_nft(query);
+  describe('Activity', () => {
+    test('get Activity Basic : Address, Chain id only', async () => {
+      const query = {
+        chain_id: 1,
+        address: _address,
+      };
+      await Portfolio.get_portfolio_activity(query);
+    });
 
-    expect(JSON.parse(res).result.length).to.equal(_limit);
+    test('get Activity Page and Limit option ', async () => {
+      const _limit = 5;
+      const query = {
+        chain_id: 1,
+        address: _address,
+        page: 1,
+        limit: _limit,
+      };
+      const res = await Portfolio.get_portfolio_activity(query);
+
+      expect(JSON.parse(res).result.length).to.equal(_limit);
+    });
   });
-
   // afterAll(() => {
   //     User.delete_user_only(test_user_id);
   //   });
