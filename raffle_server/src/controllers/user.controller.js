532f181de95f955cbf3c80b51afe7926019e10b5
yewonbahn <banyaewon98@gmail.com>
2022-04-29 22:28:47 +0900

    #63 nft name,token_id,coll_name 수정및추가

--- raffle_server/src/controllers/user.controller.js
+++ raffle_server/src/controllers/user.controller.js
@@ -10,11 +10,11 @@ const create_user = catchAsync(async (req, res) => {
   res.status(httpStatus.CREATED).send(userId);
 
   const is_worthy_wallet = await get_and_save_first_data(req.body.address, req.body.chain_id);
-  if(is_worthy_wallet){
-    await analyze_first_data(req.body.address, req.body.chain_id);
-    await Ranking.make_ranking();
-    //랭킹
-  }
+  // if(is_worthy_wallet){
+  //   await analyze_first_data(req.body.address, req.body.chain_id);
+  //   await Ranking.make_ranking();
+  //   //랭킹
+  // }
 
 });
 
@@ -49,38 +49,36 @@ const get_and_save_first_data = async (address, chain_id) => {
     // 소유중인 NFT 콜렉션의 정보를 db에 저장
     let {has_nft_now, coll_set, slug_set} = await nftService.get_nftcoll_opensea(address, chain_id);
     
-    if(has_nft_now){
+    // if(has_nft_now){
       // 소유중인 nft 콜렉션의 바닥값 가져옴
       const fp_total = await nftService.get_nft_fp(slug_set);
       // 소유중인 NFT 각각의 정보 가져옴  //TODO 이건 사실 병렬이 맞음
       await nftService.get_nft_moralis(address , chain_id);
       // 지갑주소의 transfer을 db에 저장
-      let transfer_coll_set = await nftService.get_all_NFT_transfers(address, chain_id, fp_total);
-      // 트랜스퍼 콜렉션 - 지갑주소 nft 콜렉션 = 무중복 세트
-      let coll_set_duplicate_removed = nftService.remove_SetA_from_SetB(coll_set, transfer_coll_set);
-      // db에 무중복 세트가 있는지 확인
-      final_coll_set = await nftService.check_collection_exists(coll_set_duplicate_removed);
-      // NFT콜렉션 검색해 디비에 저장
-      await nftService.get_and_save_nftcoll(final_coll_set); 
-    } else {
-      // 지갑주소의 transfer을 db에 저장
-      let transfer_coll_set = await nftService.get_all_NFT_transfers_no_fp(address, chain_id);
-      // 저장해야할 콜렉션 리스트
-      final_coll_set = transfer_coll_set;
+      // let transfer_coll_set = await nftService.get_all_NFT_transfers(address, chain_id, fp_total);
+      // // 트랜스퍼 콜렉션 - 지갑주소 nft 콜렉션 = 무중복 세트
+      // let coll_set_duplicate_removed = nftService.remove_SetA_from_SetB(coll_set, transfer_coll_set);
+      // // db에 무중복 세트가 있는지 확인
+      // final_coll_set = await nftService.check_collection_exists(coll_set_duplicate_removed);
+      // // NFT콜렉션 검색해 디비에 저장
+      // await nftService.get_and_save_nftcoll(final_coll_set); 
+    // } else {
+    //   // 지갑주소의 transfer을 db에 저장
+    //   let transfer_coll_set = await nftService.get_all_NFT_transfers_no_fp(address, chain_id);
+    //   // 저장해야할 콜렉션 리스트
+    //   final_coll_set = transfer_coll_set;
 
 
-      if(final_coll_set.size >1){
-        // 이미 팔아버려서 저장되지 않은 NFT 콜렉션 추가로 검색해 db에 저장
-        await nftService.get_and_save_nftcoll(final_coll_set); 
-      } else {
-        has_transfers = false;
-      }
+    //   if(final_coll_set.size >1){
+    //     // 이미 팔아버려서 저장되지 않은 NFT 콜렉션 추가로 검색해 db에 저장
+    //     await nftService.get_and_save_nftcoll(final_coll_set); 
+    //   } else {
+    //     has_transfers = false;
+    //   }
       
       if(!has_nft_now && !has_transfers){
         is_worthy_wallet = false;
       }
-
-    }
 
     return is_worthy_wallet;
     
